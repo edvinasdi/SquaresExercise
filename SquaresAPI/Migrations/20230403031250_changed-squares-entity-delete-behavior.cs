f1b169fd4ac7f65a910c9be5e96e0e91d83795ec
Edvinas Dičkanec <edvinas.dichkanec@gmail.com>
2023-04-03 07:11:29 +0300

    Enabled Square entity to be deleted while having foreign key constraints (DeleteBehavior.SetNull)

---
+++ SquaresAPI/Migrations/20230403031250_changed-squares-entity-delete-behavior.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SquaresAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedsquaresentitydeletebehavior : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Square_Point",
+                table: "Points");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Square_Point",
+                table: "Points",
+                column: "SquareId",
+                principalTable: "Squares",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Square_Point",
+                table: "Points");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Square_Point",
+                table: "Points",
+                column: "SquareId",
+                principalTable: "Squares",
+                principalColumn: "Id");
+        }
+    }
+}
